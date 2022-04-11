b0f66304f97dd795498409588a39e63838dd3149
Benas Volkovas <49361502+BenasVolkovas@users.noreply.github.com>
2022-04-11 19:26:43 +0300

    Update settings.py

--- Investments/settings.py
+++ Investments/settings.py
@@ -76,12 +76,10 @@ WSGI_APPLICATION = 'Investments.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if len(sys.argv) > 0 and sys.argv[1] != "collectstatic":
-    if os.getenv("DATABASE_URL", None) is None:
-        raise Exception("DATABASE_URL environment variable not defined")
-    DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
-    }
+
+DATABASES = {
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+}
 
 
 # Password validation
