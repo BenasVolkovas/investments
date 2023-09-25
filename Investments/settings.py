aea980a7fafde334c5fc7c3aa17c5585b20c96f3
Benas Volkovas <49361502+BenasVolkovas@users.noreply.github.com>
2023-09-25 21:25:34 +0300

    Update settings.py

--- Investments/settings.py
+++ Investments/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["investments-calculator.herokuapp.com"]
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
